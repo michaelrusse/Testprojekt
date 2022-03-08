432b2b7bb292c8024b763a0564deec54e55524ef
Michael Russe <michael.russe@telekom.de>
2022-03-08 15:39:27 +0100

    Änderung durchgeführt

--- src/test/java/de/telekom/sea7/DepotTest.java
+++ src/test/java/de/telekom/sea7/DepotTest.java
@@ -3,12 +3,8 @@ package de.telekom.sea7;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.EmptyStackException;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
